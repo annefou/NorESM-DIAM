09aabfaed372830a296ed5fe324304b988439cff
Anne Fouilloux <annefou@geo.uio.no>
2019-12-20 14:33:41 +0100

    use xarray and add command line arguments

--- make_lbc_file.py
+++ make_lbc_file.py
@@ -8,174 +8,117 @@
 #-----------------------------------------------------------------------
 
 # Modules needed:
-from netCDF4 import Dataset
+# Modules needed
 import numpy as np
-from pathlib import Path
-import shutil
-import sys
+import pandas as pd
+import xarray as xr
+import cftime
+from optparse import OptionParser
 
-#-----------------------------------------------------------------------
+def make_lbc_file(filename_in, couple4file, filename_out, nyears, start_year, select_year):
+    """ Create a new netCDF file with nyears starting from start_year.
+        All variable values except CO2_LBC are filled with select_year values that are repeated 
+        nyears from start_year. CO2_LBC values are filled with the CO2 values from DIAM simulation
+        that are contained in couple4file (3rd column).
+        """
+    dset_in = xr.open_dataset(filename_in, decode_times=True, use_cftime=True)
+    # Select a single year (start_year)
+    dset_tmp = dset_in.sel(time=slice(cftime.DatetimeGregorian(start_year, 1, 1),
+                                      cftime.DatetimeGregorian(start_year+1, 1, 1)))
+    # Compute new time and time bounds
+    time_start = dset_in.sel(time=cftime.DatetimeGregorian(start_year, 1, 1), method='backfill').time
+    new_time = [cftime.DatetimeGregorian(year, time_start.dt.month, time_start.dt.day) 
+                for year in range(start_year, start_year+nyears)]
+    new_time_bnds = [[cftime.DatetimeGregorian(year, 1, 1), cftime.DatetimeGregorian(year+1, 1, 1)] 
+                     for year in range(start_year, start_year+nyears)]
+    new_date = [float(cftime.DatetimeGregorian(year, time_start.dt.month, time_start.dt.day).strftime(format="%Y%m%d")) 
+                for year in range(start_year, start_year+nyears)]
+    # Reindex with new times
+    dset_out = dset_tmp.reindex({"time":new_time})
+    # Fill time and time_bnds
+    dset_out['time'] = dset_out.time
+    dset_out['time_bnds'][:,:] = new_time_bnds
+    dset_out['N2O_LBC'][:,:] = dset_in.sel(time=slice(cftime.DatetimeGregorian(select_year, 1, 1),
+                                      cftime.DatetimeGregorian(select_year+1, 1, 1))).N2O_LBC.values
+    dset_out['CH4_LBC'][:,:] = dset_in.sel(time=slice(cftime.DatetimeGregorian(select_year, 1, 1),
+                                      cftime.DatetimeGregorian(select_year+1, 1, 1))).CH4_LBC.values
+    dset_out['CFC11eq_LBC'][:,:] = dset_in.sel(time=slice(cftime.DatetimeGregorian(select_year, 1, 1),
+                                      cftime.DatetimeGregorian(select_year+1,1, 1, 1))).CFC11eq_LBC.values
+    dset_out['CF2CL2_LBC'][:,:] = dset_in.sel(time=slice(cftime.DatetimeGregorian(select_year, 1, 1),
+                                      cftime.DatetimeGregorian(select_year+1, 1, 1))).CF2CL2_LBC.values
+    dset_out['date'][:] = new_date
+    
+    # Read CO2 values from DIAM simulation
+    data = pd.read_csv(couple4file, header=None, sep="\s+")
+    # Convert CO2 values (3rd column) from GtC to ppm:
+    GtC_per_ppm = 2.13
+    atm_co2 = data[3].values / GtC_per_ppm
+    # Convert from ppm to mol/mol:
+    atm_co2 = atm_co2 * 1e-6
+    # Fill CO2_LBC with these new CO2 values
+    # - The first 4 values are constant and set to the first CO2 value from DIAM simulation 
+    #   (start_year to select_year)
+    # - All other years are filled with CO2 values from couple4.t
+    dset_out['CO2_LBC'][0:4,:] = atm_co2[0]
+    for i in range(len(dset_out.time)-4):
+        dset_out['CO2_LBC'][4+i,:] = atm_co2[i]
+    
+    # Save to output netCDF file
+    dset_out.to_netcdf(filename_out)
+    
 
-filename_original = 'LBC_1750-2015_CMIP6_GlobAnnAvg_c180926.nc'
-filename  = 'lbc_short_test_without_tax_from_2000_i1.nc'
-years = 269		# number of years in file (must be more than the 266 time dim in the original file)
+def main():
+    usage = """usage: %prog --start_year=YYYY --reference_year=YYYY --nyears=n
+                            --input=input_file.nc --output=output_file.nc
+                            --couple=diam.txt"""
+    parser = OptionParser(usage=usage)
+    parser.add_option("-s", "--start_year", dest="start_year",
+                      help="start year YYYY", metavar="start_year" )
+    parser.add_option("-r", "--reference_year", dest="reference_year",
+                      help="reference year YYYY", metavar="reference_year" )
+    parser.add_option("-n", "--nyears", dest="nyears",
+                      help="number of years", metavar="nyears")
+    parser.add_option("--input", dest="input_file",
+                      help="Input netCDF file with reference data", metavar="input")
+    parser.add_option("--output", dest="output_file",
+                      help="Output netCDF filename", metavar="output")
+    parser.add_option("--couple", dest="couple_diam_file",
+                      help="DIAM txt output file", metavar="couple")
 
+    (options, args) = parser.parse_args()
 
-# Delete if file exists:
-file = Path(filename)
-if file.exists():
-	file.unlink()
+    if not options.start_year:
+        parser.error("start year must be specified!")
+    else:
+        start_year = int(options.start_year)
+    
+    if not options.reference_year:
+        reference_year = start_year
+    else:
+        reference_year = int(options.reference_year)
 
-shutil.copyfile(filename_original, filename)
-
-#------------------------------------------------------------------------
-
-# Read in original variables:
-original_file 		= Dataset(filename_original, 'r')
-original_time 		= original_file.variables['time'][246:]			# 1996 = 246
-original_date 		= original_file.variables['date'][246:]			# 1996 = 246
-original_time_bnds 	= original_file.variables['time_bnds'][246:,:]	# 1996 = 246
-original_N2O		= original_file.variables['N2O_LBC'][250,:]		# only read 2000 
-original_CH4		= original_file.variables['CH4_LBC'][250,:]		# only read 2000 
-original_CFC11eq	= original_file.variables['CFC11eq_LBC'][250,:]	# only read 2000 
-original_CF2CL2		= original_file.variables['CF2CL2_LBC'][250,:]	# only read 2000 
-original_file.close() 
-
-#------------------------------------------------------------------------
-
-# CHANGE ALL TIME RELATED VARIABLES:
-print(original_time)
-print(original_date)
-
-# Calculate how time and date changes between time steps:
-time_diff 		= np.array([original_time[1] - original_time[0], original_time[2] - original_time[1], \
-							original_time[3] - original_time[2], original_time[4] - original_time[3]])
-date_diff 		= original_date[1] - original_date[0]
-time_bnds_diff 	= np.array([original_time_bnds[1,:] - original_time_bnds[0,:], original_time_bnds[2,:] - original_time_bnds[1,:], \
-							original_time_bnds[3,:] - original_time_bnds[2,:], original_time_bnds[4,:] - original_time_bnds[3,:]])
-
-
-# Make new time list:
-new_time = [original_time[0]]
-
-for i in range(int(years/4)):
-	new_time.append(new_time[-1] + time_diff[0])
-	new_time.append(new_time[-1] + time_diff[1])
-	new_time.append(new_time[-1] + time_diff[2])
-	new_time.append(new_time[-1] + time_diff[3])
-
-# Make new date list:
-new_date = [original_date[0]]
-
-for i in range(years - 1):
-	new_date.append(int(new_date[-1] + date_diff))
-
-# Make new time bounds array:
-new_time_bnds = np.zeros((years,2), dtype=np.float32)
-new_time_bnds[0,:] = original_time_bnds[0,:]
-
-idx = 1
-for i in range(int(years/4)):
-	new_time_bnds[idx,:] = new_time_bnds[idx-1,:] + time_bnds_diff[0,:]
-	idx +=1
-	new_time_bnds[idx,:] = new_time_bnds[idx-1,:] + time_bnds_diff[1,:]
-	idx +=1
-	new_time_bnds[idx,:] = new_time_bnds[idx-1,:] + time_bnds_diff[2,:]
-	idx +=1
-	new_time_bnds[idx,:] = new_time_bnds[idx-1,:] + time_bnds_diff[3,:]
-	idx +=1
-
-
-# Check that length of new time and time bounds equal number of years:
-if len(new_time) != years:
-	sys.exit("Length of time list is different from number of years specified.")
-elif new_time_bnds.shape[0] != years:
-	sys.exit("Length of time bounds array is different from number of years specified.")
-
-# Open new file for writing:
-file_lbc = Dataset(filename, 'r+')
-
-# Fill time and date variable with values:
-file_lbc.variables['time'][:] 		= new_time	
-file_lbc.variables['date'][:] 		= new_date
-file_lbc.variables['time_bnds'][:] 	= new_time_bnds	
-
-file_lbc.close()	# closing file so that the time dimension is updated				
-
-#------------------------------------------------------------------------
-
-# REPEAT 2000 VALUES FOR CH4, N2O, CFC11_eq AND CF2CL2 FOR ALL YEARS:
-
-# Create arrays of correct dimension:
-CH4 	= np.zeros((years, original_CH4.shape[0]), dtype=np.float32)
-N2O 	= np.zeros((years, original_CH4.shape[0]), dtype=np.float32)
-CFC11eq = np.zeros((years, original_CH4.shape[0]), dtype=np.float32)
-CF2CL2 	= np.zeros((years, original_CH4.shape[0]), dtype=np.float32)
-
-for i in range(original_CH4.shape[0]):
-	CH4[:,i] 		= original_CH4[i]
-	N2O[:,i] 		= original_N2O[i]
-	CFC11eq[:,i] 	= original_CFC11eq[i]
-	CF2CL2[:,i] 	= original_CF2CL2[i]
-
-print(original_CH4[0])
-print(original_N2O[0])
-print(original_CFC11eq[0])
-print(original_CF2CL2[0])
-
-# Reopening files:
-file_lbc = Dataset(filename, 'r+')
-
-# Repeat values of other variables than those for time and CO2:
-
-file_lbc.variables['CH4_LBC'][:] 		= CH4
-file_lbc.variables['N2O_LBC'][:] 		= N2O
-file_lbc.variables['CFC11eq_LBC'][:] 	= CFC11eq
-file_lbc.variables['CF2CL2_LBC'][:] 	= CF2CL2
-
-
-#------------------------------------------------------------------------
-
-# CO2 VALUES:
-
-# Read in CO2 values from DIAM file:
-with open('/home/jennybj/Documents/DIAM/couple4.t') as myfile:
-	lines=myfile.readlines()
-
-# Extract the wanted values:
-atm_co2 = []
-
-for line in lines:
-	atm_co2.append(float(line.split()[3]))	
-
-# Convert from GtC to ppm:
-GtC_per_ppm = 2.13
-atm_co2 = [line/GtC_per_ppm for line in atm_co2]
-
-# Convert from ppm to mol/mol:
-atm_co2 = [line*1e-6 for line in atm_co2]
-
-# Extract the wanted years (index 0 = 1990) (adding 4 extra years):
-co2vmr = [atm_co2[0]]*4 + atm_co2[0:years-4]
-
-#------------------------------------------------------------------------
-
-print(co2vmr[0:80])
-
-# Make (time,lat) array for CO2 concentrations:
-CO2 = np.zeros((years, original_CH4.shape[0]), dtype=np.float32)
-
-for i in range(years):
-	CO2[i,:] = co2vmr[i]
-
-# Check that the number of timesteps is correct:
-if not len(co2vmr) == years:
-	sys.exit("Number of timesteps in file and CO2 array is not the same!")
-
-# Write CO2 values to file:
-
-file_lbc.variables['CO2_LBC'][:] = CO2
-
-#-------------------------------------------------------------------------
-
-file_lbc.close()
+    if not options.nyears:
+        parser.error("Number of years must be specified!")
+    else:
+        nyears = int(options.nyears)
+    
+    if not options.input_file:
+        input_file = "LBC_1750-2015_CMIP6_GlobAnnAvg_c180926.nc"
+    else:
+        input_file = options.input_file
+        
+    if not options.output_file:
+        output_file = "lbc_short_test_without_tax_from_" + str(reference_year) + "_i1.nc"
+    else:
+        output_file = options.output_file
+        
+    if not options.couple_diam_file:
+        couple_diam_file = "couple4.t"
+    else:
+        couple_diam_file = options.couple_diam_file
+        
+        
+    make_lbc_file(input_file, couple_diam_file, output_file, nyears, start_year, reference_year)
+    
+if __name__ == "__main__":
+    main()
